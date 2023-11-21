574b263888b80f974d975319967fa23296ee0f30
sunlyhuor <jkhmer1111@gmail.com>
2023-11-21 04:17:22 -0500

    added by devel

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Huor");
         System.out.println("Hello Deth");
+        System.out.println("Edit by delvelop");
     }
 
     public static void testing(){
