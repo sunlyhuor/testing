17bd873ec12cb6a9b82dd129abd27e10e21ce0bf
sunlyhuor <jkhmer1111@gmail.com>
2023-11-21 04:19:24 -0500

    added by huor

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Huor");
+        System.out.println("Added");
         System.out.println("Hello Deth");
         System.out.println("Edit by delvelop");
     }
